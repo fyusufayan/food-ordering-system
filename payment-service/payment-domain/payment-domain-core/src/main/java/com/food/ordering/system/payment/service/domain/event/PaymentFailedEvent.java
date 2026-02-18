8fd79ab029a63c7deab2f90ad5504a2d91dfb697
Faik Yusuf Ayan <faik.ayan@tubitak.gov.tr>
2026-02-18 16:06:24 +0300

    payment application service module implementation

--- payment-service/payment-domain/payment-domain-core/src/main/java/com/food/ordering/system/payment/service/domain/event/PaymentFailedEvent.java
+++ payment-service/payment-domain/payment-domain-core/src/main/java/com/food/ordering/system/payment/service/domain/event/PaymentFailedEvent.java
@@ -1,12 +1,26 @@
 package com.food.ordering.system.payment.service.domain.event;
 
+import com.food.ordering.system.domain.event.publisher.DomainEventPublisher;
 import com.food.ordering.system.payment.service.domain.entity.Payment;
 
 import java.time.ZonedDateTime;
 import java.util.List;
 
 public class PaymentFailedEvent extends PaymentEvent{
-    public PaymentFailedEvent(Payment payment, ZonedDateTime createdAt, List<String> failedMessages) {
+
+    private final DomainEventPublisher<PaymentFailedEvent> paymentFailedEventDomainEventPublisher;
+
+    public PaymentFailedEvent(Payment payment,
+                              ZonedDateTime createdAt,
+                              List<String> failedMessages,
+                              DomainEventPublisher<PaymentFailedEvent> paymentFailedEventDomainEventPublisher) {
         super(payment, createdAt, failedMessages);
+        this.paymentFailedEventDomainEventPublisher = paymentFailedEventDomainEventPublisher;
+    }
+
+
+    @Override
+    public void fire() {
+        paymentFailedEventDomainEventPublisher.publish(this);
     }
 }
