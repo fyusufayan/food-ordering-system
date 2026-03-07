740a011f7cc759e5b3055945128a83326f283fde
Faik Yusuf Ayan <faik.ayan@tubitak.gov.tr>
2026-03-07 16:37:51 +0300

    updated order payment saga and order approval saga for outbox pattern

--- order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/OrderCreateCommandHandler.java
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/OrderCreateCommandHandler.java
@@ -4,35 +4,53 @@ import com.food.ordering.system.order.service.domain.dto.create.CreateOrderComma
 import com.food.ordering.system.order.service.domain.dto.create.CreateOrderResponse;
 import com.food.ordering.system.order.service.domain.event.OrderCreatedEvent;
 import com.food.ordering.system.order.service.domain.mapper.OrderDataMapper;
-import com.food.ordering.system.order.service.domain.ports.output.message.publisher.payment.OrderCreatedPaymentRequestMessagePublisher;
+import com.food.ordering.system.order.service.domain.outbox.scheduler.payment.PaymentOutboxHelper;
+
+import com.food.ordering.system.outbox.OutboxStatus;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
 
 @Slf4j
 @Component
 public class OrderCreateCommandHandler {
 
-
     private final OrderCreateHelper orderCreateHelper;
     private final OrderDataMapper orderDataMapper;
-    private final OrderCreatedPaymentRequestMessagePublisher orderCreatedPaymentRequestMessagePublisher;
+    private final PaymentOutboxHelper paymentOutboxHelper;
+    private final OrderSagaHelper orderSagaHelper;
 
     public OrderCreateCommandHandler(OrderCreateHelper orderCreateHelper,
                                      OrderDataMapper orderDataMapper,
-                                     OrderCreatedPaymentRequestMessagePublisher orderCreatedPaymentRequestMessagePublisher) {
+                                     PaymentOutboxHelper paymentOutboxHelper,
+                                     OrderSagaHelper orderSagaHelper) {
         this.orderCreateHelper = orderCreateHelper;
         this.orderDataMapper = orderDataMapper;
-        this.orderCreatedPaymentRequestMessagePublisher = orderCreatedPaymentRequestMessagePublisher;
+        this.paymentOutboxHelper = paymentOutboxHelper;
+        this.orderSagaHelper = orderSagaHelper;
     }
 
+    @Transactional
     public CreateOrderResponse createOrder(CreateOrderCommand createOrderCommand) {
 
         OrderCreatedEvent orderCreatedEvent = orderCreateHelper.persistOrder(createOrderCommand);
         log.info("Order with id: {} is created!", orderCreatedEvent.getOrder().getId().getValue());
+        CreateOrderResponse createOrderResponse = orderDataMapper.orderToCreateOrderResponse(
+                orderCreatedEvent.getOrder(),
+                "Order Created Successfully");
 
-        orderCreatedPaymentRequestMessagePublisher.publish(orderCreatedEvent);
-        return orderDataMapper.orderToCreateOrderResponse(orderCreatedEvent.getOrder(), "Order Created Successfully");
+        paymentOutboxHelper.savePaymentOutboxMessage(
+                orderDataMapper.orderCreatedEventToOrderPaymentEventPayload(orderCreatedEvent),
+                orderCreatedEvent.getOrder().getStatus(),
+                orderSagaHelper.orderStatusToSagaStatus(orderCreatedEvent.getOrder().getStatus()),
+                OutboxStatus.STARTED,
+                UUID.randomUUID()
+        );
+        log.info("Returning CreateOrderResponse with order id: {}", orderCreatedEvent.getOrder().getId());
 
+        return createOrderResponse;
     }
 
 }
