4395629ddfed1f8204406316798382c502d0e93c
Faik Yusuf Ayan <faik.ayan@tubitak.gov.tr>
2026-03-08 10:06:59 +0300

    refaktör order messaging for outbox pattern

--- infrastructure/kafka/kafka-producer/src/main/java/com/food/ordering/system/kafka/producer/KafkaMessageHelper.java
+++ infrastructure/kafka/kafka-producer/src/main/java/com/food/ordering/system/kafka/producer/KafkaMessageHelper.java
@@ -1,5 +1,9 @@
 package com.food.ordering.system.kafka.producer;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.food.ordering.system.order.service.domain.exception.OrderDomainException;
+import com.food.ordering.system.outbox.OutboxStatus;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.producer.RecordMetadata;
 import org.springframework.kafka.support.SendResult;
@@ -11,22 +15,40 @@ import java.util.function.BiConsumer;
 @Component
 public class KafkaMessageHelper {
 
-    public<T> BiConsumer<SendResult<String, T>, Throwable>
-    getKafkaCallback(String responseTopicName, T  avroModel, String orderId, String avroModelName) {
+    private final ObjectMapper objectMapper;
+    public KafkaMessageHelper(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
+    public <T> T getOrderEventPayload(String payload, Class<T> outputType) {
+        try {
+            return objectMapper.readValue(payload, outputType);
+        }catch ( JsonProcessingException e){
+            log.error("Could not read {} object: ", outputType.getName(), e);
+            throw new OrderDomainException("Could not read " + outputType.getName() + " object: " + e);
+        }
+    }
+
+
+    public<T, U> BiConsumer<SendResult<String, T>, Throwable>
+    getKafkaCallback(String responseTopicName, T  avroModel, U outboxMessage,
+                     BiConsumer<U, OutboxStatus> outboxCallback,
+                     String orderId, String avroModelName) {
 
         return (result, ex) -> {
 
             if (ex != null) {
-                log.error("Error while sending {} message {} to topic {}",
+                log.error("Error while sending {} with message: {} and outbox type: {} to topic {}",
                         avroModelName,
-                        avroModel,
+                        avroModel.toString(),
+                        outboxMessage.getClass().getName(),
                         responseTopicName,
                         ex);
+                outboxCallback.accept(outboxMessage, OutboxStatus.FAILED);
                 return;
             }
 
             RecordMetadata metadata = result.getRecordMetadata();
-
             log.info(
                     "Received successful response from kafka for order id: {} topic: {} partition: {} offset: {} timestamp: {}",
                     orderId,
@@ -35,6 +57,7 @@ public class KafkaMessageHelper {
                     metadata.offset(),
                     metadata.timestamp()
             );
+            outboxCallback.accept(outboxMessage, OutboxStatus.COMPLETED);
         };
 
 
